c9bae5ad861eee8961712ad14300d0e0669c75b9
Jordan McDaniel <jdon8600@gmail.com>
2023-04-19 21:53:19 -0400

    complete card component (can change)

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,14 +1,20 @@
 import React from "react";
-import classes from "./card.module.css"
+import classes from "./card.module.css";
 
-const Card = ({cvalue, csuit}: {cvalue:string, csuit:string}) => {
-    const combo = `${cvalue}${csuit}`;
-    const color = (csuit === '♥' || csuit === '♦')? "redCard" : "card";
-    return (
-        <div className={classes[color]}>
-            {combo}
-        </div>
-    )
-}
+const Card = ({
+  cvalue,
+  csuit,
+}: {
+  cvalue: string | number;
+  csuit: string;
+}) => {
+  const combo = cvalue ? `${cvalue}${csuit}` : null;
+  const color = csuit === "♥" || csuit === "♦" ? "redCard" : "card";
+  return (
+    <td>
+      <div className={classes[color]}>{combo}</div>
+    </td>
+  );
+};
 
 export default Card;
